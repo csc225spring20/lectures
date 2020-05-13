daf27e75fd6c7bc7232e4f76bf2439f61a792439
Christopher Siu <christopher@siu.sh>
2020-05-13 14:16:09 -0700

    Added example of changing the behavior of Privilege Violations.

--- 05.13_Interrupts/privilege.asm
+++ 05.13_Interrupts/privilege.asm
@@ -1,6 +1,14 @@
-EX_PRIV     LEA R0, EX_PRIV_MSG
-            PUTS
-            HALT
+            .ORIG x300
 
+EX_PRIV     ST R0, EX_PRIV_R0
+            LEA R0, EX_PRIV_MSG
+            PUTS
+            LD R0, EX_PRIV_R0
+            RTI
+
+EX_PRIV_R0
+ .FILL x0000
 EX_PRIV_MSG
- .STRINGZ "\n\n--- Access Violation ---\n\n"
+ .STRINGZ "\n\n--- You'd better not do that again! ---\n\n"
+
+            .END
