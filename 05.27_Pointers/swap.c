a1efde66b638b6e27eb4f9ca409178c2450789f2
Christopher Siu <christopher@siu.sh>
2020-05-29 08:02:24 -0700

    Example of using pointers.

--- 05.27_Pointers/swap.c
+++ 05.27_Pointers/swap.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-void swap(int, int);
+void swap(int *, int *);
 
 int main(void) {
     int z = 1, w = 2;
-    swap(z, w);
+    swap(&z, &w);
 
     printf("z: %d, w: %d\n", z, w);
 }
 
-void swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
+/* Passing pointers allows us to emulate pass-by-reference; "swap" has access
+ *  to the local variables in "main". */
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
